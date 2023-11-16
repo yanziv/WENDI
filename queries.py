a098dbe1d611123c4d77a9e90ac83da2f0247ed9
Emma Lim <el110@cs.wellesley.edu>
2023-11-16 11:09:40 -0500

    add show_reviews query function to queries.py

--- queries.py
+++ queries.py
@@ -4,6 +4,14 @@ def show_rooms(conn, dormid):
     '''return all rooms with specified dormid as the hid'''
     curs = dbi.dict_cursor(conn)
     curs.execute('''
-        select number from room where hid = %s
+        select number from room2 where hid = %s
     ''', [dormid])
+    return curs.fetchall()
+
+def show_reviews(conn, roomid):
+    '''return all reviews made for specified room'''
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select * from review where rid = %s
+    ''', [roomid])
     return curs.fetchall()
