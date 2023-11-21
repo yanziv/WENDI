ed22f12a745c2f161681de04270cb2c34b0e53dc
Emma Lim <el110@cs.wellesley.edu>
2023-11-21 08:53:08 -0500

    routing dorm and room

--- queries.py
+++ queries.py
@@ -5,7 +5,7 @@ def show_rooms(conn, dormid):
     '''return all rooms with specified dormid as the hid'''
     curs = dbi.dict_cursor(conn)
     curs.execute('''
-        select number from room2 where hid = %s
+        select number from room where hid = %s
     ''', [dormid])
     return curs.fetchall()
 
