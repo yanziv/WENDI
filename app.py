2862b7205810269d2856dd73ecb173117f01ad5a
Veronica Lin <yl102@cs.wellesley.edu>
2023-11-24 12:36:36 -0500

    modified review form

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ from flask import (
     jsonify,
 )
 from werkzeug.utils import secure_filename
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -51,11 +52,14 @@ EAST_SIDE_COMPLEX = ['Bates','Freeman','McAfee']
 SD_AND_SMALL_HALLS = ['Casa Cervantes','French House','Stone-Davis']
 ALL_HALLS = THE_QUINT + TOWER_COMPLEX + EAST_SIDE_COMPLEX + SD_AND_SMALL_HALLS
 
+
 @app.route('/browse_all/', methods=["GET", "POST"])
 def landing():
     if request.method == 'GET':
+        flash('Not Yet Implemented for the Draft Version!')
         return render_template('landing.html')
     # else:
+    # id is post id and then uid should be retrieved from cached username
     #     if request.form['submit'] == 'All Halls':
     #         return render_template('landing.html')
     #     elif request.form['submit'] == 'Tower Complex':
@@ -73,10 +77,42 @@ def landing():
 
 @app.route('/review/', methods=["GET", "POST"])
 def review():
+    conn = dbi.connect()
     if request.method == 'GET':
-        return render_template('review.html')
+        all_dorms = queries.get_all_dorms(conn)
+        return render_template('form.html',dorms = all_dorms)
  
-
+    else: # insert review into db
+        dorm = request.form.get('res-hall')
+        hall_id = queries.get_hid_given_hall_name(conn,dorm)
+        rid = request.form.get('rid')
+        overallRating = request.form.get('overall')
+        startDate = request.form.get('start-date')
+        year = request.form.get('year')
+        month = request.form.get('month')
+        if year != '0':
+            length = year + ' year and ' + month + ' months'
+        else:
+            length = month + ' months'
+        size = request.form.get('size')
+        storage = request.form.get('storage')
+        ventilation = request.form.get('ventilation')
+        cleanliness = request.form.get('cleanliness')
+        bathroom = request.form.get('bathroom')
+        accessibility = request.form.get('accessibility')
+        sunlight = request.form.get('sunlight')
+        bugs = request.form.get('bugs')
+        window = request.form.get('window')
+        noise = request.form.get('noise')
+        comments = request.form.get('comments')
+        submission_time = datetime.now()
+        # queries.insert_review(conn,new_tt,new_title,new_release,addedby)
+        flash('Thank you for submitting a review!')
+        # render the html page for the room review user just submitted
+        return render_template( url_for('room',hid = hall_id,number = rid) )
+#   id | uid  | rid  | rating | startTime  | lengthOfStay | sizeScore | storageScore | 
+# ventScore | cleanScore | bathroomScore | accessibilityScore | sunlightScore | bugScore | 
+# windowScore | noiseScore | comment | hasMedia | timePosted
 
 @app.route("/dorm/<hid>")
 def dorm(hid):
--- queries.py
+++ queries.py
@@ -1,5 +1,33 @@
 import cs304dbi as dbi
 
+def get_all_dorms(conn):
+    """
+    Returns info regarding all residential halls at Wellesley.
+    """
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select * from hall''')
+    return curs.fetchall()
+
+def get_hid_given_hall_name(conn,hall_name):
+    """
+    Returns the three-letter encoding hid of a given residential hall.
+    """
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''
+        select id from hall
+        where name = %s''', [hall_name])
+    return curs.fetchone()
+
+def review(conn,tt,title,release,addedby):
+    """
+    After checking tt doesn't exist in the movie table in wmdb,
+    proceed to insert this new movie into wmdb.
+    """
+    curs = dbi.dict_cursor(conn)
+    curs.execute('''insert into movie(tt,title,release,addedby)
+                    values (%s, %s, %s, %s)''',[tt,title,release,addedby])
+    conn.commit()
 
 def show_rooms(conn, hall_id):
     '''return all rooms with specified hall_id as the hid'''
